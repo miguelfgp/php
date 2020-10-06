315ef09e376bf709caa35337a3eb77054c985d4e
Miguel <miguel_fgp@hotmail.es>
2020-10-06 14:23:20 +0200

    Ejercicios 1 y 2 de arrays hechos

---
+++ Tema 1 - Arrays/Ejercicios/Ejercicio1.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href = "Ejercicio1.php">Ejercicio 1</a></li>
+        <li><a href = "Ejercicio2.php">Ejercicio 2</a></li>
+        <li><a href = "Ejercicio3.php">Ejercicio 3</a></li>
+        <li><a href = "Ejercicio4.php">Ejercicio 4</a></li>
+        <li><a href = "Ejercicio5.php">Ejercicio 5</a></li>
+        <li><a href = "Ejercicio6.php">Ejercicio 6</a></li>
+        <li><a href = "Ejercicio7.php">Ejercicio 7</a></li>
+        <li><a href = "Ejercicio8.php">Ejercicio 8</a></li>
+    </ul>
+</nav>
+<h1>Ejercicios Tema 1 (Arrays) -  Miguel FGP</h1>
+
+<h3>Almacenar en un vector los 10 primeros números pares. Imprimir cada uno en una línea</h3>
+<?php
+
+    $index = 0;
+
+    for ($i = 2; $i <= 10; $i = $i + 2){    
+        $numeros[$index] = $i;
+        $index++;
+    }
+
+    foreach ($numeros as $numero){
+        echo '<p>' . $numero . '</p>';
+    }
+
+?>
+</body>
+</html>
---
+++ Tema 1 - Arrays/Ejercicios/Ejercicio2.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href = "Ejercicio1.php">Ejercicio 1</a></li>
+        <li><a href = "Ejercicio2.php">Ejercicio 2</a></li>
+        <li><a href = "Ejercicio3.php">Ejercicio 3</a></li>
+        <li><a href = "Ejercicio4.php">Ejercicio 4</a></li>
+        <li><a href = "Ejercicio5.php">Ejercicio 5</a></li>
+        <li><a href = "Ejercicio6.php">Ejercicio 6</a></li>
+        <li><a href = "Ejercicio7.php">Ejercicio 7</a></li>
+        <li><a href = "Ejercicio8.php">Ejercicio 8</a></li>
+    </ul>
+</nav>
+<h1>Ejercicios Tema 1 (Arrays) -  Miguel FGP</h1>
+
+<h3>Escriba un programa que muestre una tirada de dado al azar y escriba en letras el valor obtenido (Usar rand(veces, num_max)).</h3>
+<?php
+
+    $numero_aleatorio = rand(1, 6);
+
+    $numeros_escritos = array(
+        '1' => 'Uno',
+        '2' => 'Dos',
+        '3' => 'Tres',
+        '4' => 'Cuatro',
+        '5' => 'Cinco',
+        '6' => 'Seis'
+    );
+    
+    echo '<p>Ha salido el número ' . $numeros_escritos[$numero_aleatorio] . '</p>'
+
+?>
+</body>
+</html>
